5d55b5c73ecb55cb228350792e8d1ad1d0519c1f
danny.chen <danny.chencc@ef.com>
2013-04-18 16:11:33 +0800

    1

---
+++ Core/ElasticSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core
+{
+    public class ElasticSearch
+    {
+        public static void Index(string key, object value)
+        { }
+
+        public static object Take(string key)
+        {
+            return null;
+        }
+    }
+}
---
+++ PhysicalDiskFileParser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Core;
+
+namespace PhysicalDiskFileParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = args[0];
+            Console.WriteLine("start parsing all files recursively at " + root);
+
+            var ignoreList = new[] { ' ', '.', '(', ')', '[', ']', '{', '}', '+', '-', '*', '/', ';' };
+            foreach (var file in Directory.EnumerateFiles(root))
+            {                
+                foreach (var line in File.ReadLines(file))
+                {
+                    var words = line.Split(ignoreList, StringSplitOptions.RemoveEmptyEntries).Distinct();
+                    foreach (var word in words)
+                    {
+                        ElasticSearch.Index(word, file);
+                    }
+                }
+            } 
+
+            Console.WriteLine("finished");
+        }
+    }
+}
