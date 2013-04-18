2c27b388f33919e65c73ec34cddc260919729648
danny.chen <danny.chencc@ef.com>
2013-04-18 16:56:45 +0800

    111

---
+++ PhysicalDiskFileParser/Keyword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhysicalDiskFileParser
+{
+    public class Keyword
+    {
+        public string Word { get; set; }
+
+        public string PhysicalPath { get; set; }
+
+        public HashSet<int> LineNumbers { get; set; }
+    }
+}
--- PhysicalDiskFileParser/Program.cs
+++ PhysicalDiskFileParser/Program.cs
@@ -14,16 +14,38 @@ namespace PhysicalDiskFileParser
             var root = args[0];
             Console.WriteLine("start parsing all files recursively at " + root);
 
-            var ignoreList = new[] { ' ', '.', '(', ')', '[', ']', '{', '}', '+', '-', '*', '/', ';' };
+            var ignoreList = new[] { ' ', '.', ',', '(', ')', '[', ']', '{', '}', '+', '-', '*', '/', ';', '<', '>', '"' };
             foreach (var file in Directory.EnumerateFiles(root))
-            {                
+            {
+                var keywords = new Dictionary<string, Keyword>();
+                var lineNumber = 0;
                 foreach (var line in File.ReadLines(file))
                 {
                     var words = line.Split(ignoreList, StringSplitOptions.RemoveEmptyEntries).Distinct();
+                    lineNumber++;
+
                     foreach (var word in words)
                     {
-                        ElasticSearch.Index(word, file);
+                        if (!keywords.ContainsKey(word))
+                        {
+                            keywords.Add(word, new Keyword
+                            {
+                                Word = word,
+                                PhysicalPath = file,
+                                LineNumbers = new HashSet<int> { lineNumber }
+                            });
+                        }
+                        else
+                        {
+                            keywords[word].LineNumbers.Add(lineNumber);
+                        }
                     }
+                }
+
+                foreach (var pair in keywords)
+                {
+                    //ElasticSearch.Index(pair.Key, pair.Value);
+                    Console.WriteLine("keyword: {0}  path: {1}  lines: {2}", pair.Key, pair.Value.PhysicalPath, string.Join(",", pair.Value.LineNumbers));
                 }
             } 
 
